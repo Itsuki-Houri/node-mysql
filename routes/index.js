38d47fdf99cdac1822e06e225754a5459da3e780
Itsuki-Houri <j2336@matsue-ct.ac.jp>
2026-06-11 06:14:49 +0000

    真・ToDoタスクを表示させよう

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ let todos=[];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDo App' });
+  res.render('index', { title: 'ToDo App' ,todos:todos});
 });
 
 
 router.post('/', function(req, res, next) {
   const todo = req.body.add;
   todos.push(todo);
-  res.render('index', { title: 'ToDo App' });
+  res.render('/' );
 });
 
 
