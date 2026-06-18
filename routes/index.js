0ef79ee76fcfde7737d93eda19dc2103c67aabac
Itsuki-Houri <j2336@matsue-ct.ac.jp>
2026-06-18 06:43:36 +0000

    ToDoアプリで必要なデータ構造を検討し、ToDoタスクを管理するテーブルを作成しよう/mysqlモジュールを用いてデータベースに接続できるようにしよう

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
 
 let todos=[];
 
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,9 +20,16 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
   todos.push(todo);
-  res.render('/' );
+  res.redirect('/' );
 });
 
 
